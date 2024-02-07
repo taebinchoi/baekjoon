b79fdeea7f56d5b5500105efd611ae8594e437b5
pluto7 <141066610+taebinchoi@users.noreply.github.com>
2024-02-08 04:30:16 +0900

    [Unrated] Title: Rick, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Unrated/15639. Rick/Rick.py
@@ -0,0 +1,7 @@
+N = str(input())
+rick = ['Give you up', 'Let you down', 'Run around and desert you', 
+        'Make you cry', 'Say goodbye', 'Tell a lie and hurt you']
+if N in rick:
+    print("NO")
+else:
+    print("YES")
