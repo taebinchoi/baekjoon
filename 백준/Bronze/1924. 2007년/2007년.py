10d44940312209245c4a2b3a0d8a8fad5cd95ea0
pluto7 <141066610+taebinchoi@users.noreply.github.com>
2024-02-08 04:22:36 +0900

    [Bronze I] Title: 2007년, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1924. 2007년/2007년.py
@@ -0,0 +1,181 @@
+X, Y = map(int,input().split())
+if X == 1:
+    if Y%7 == 1:
+        print('MON')
+    elif Y%7 == 2:
+        print('TUE')
+    elif Y%7 == 3:
+        print('WED')
+    elif Y%7 == 4:
+        print('THU')
+    elif Y%7 == 5:
+        print('FRI')
+    elif Y%7 == 6:
+        print('SAT')
+    else:
+        print('SUN')
+elif X == 2:
+    if Y%7 == 5:
+        print('MON')
+    elif Y%7 == 6:
+        print('TUE')
+    elif Y%7 == 0:
+        print('WED')
+    elif Y%7 == 1:
+        print('THU')
+    elif Y%7 == 2:
+        print('FRI')
+    elif Y%7 == 3:
+        print('SAT')
+    else:
+        print('SUN')
+elif X == 3:
+    if Y%7 == 5:
+        print('MON')
+    elif Y%7 == 6:
+        print('TUE')
+    elif Y%7 == 0:
+        print('WED')
+    elif Y%7 == 1:
+        print('THU')
+    elif Y%7 == 2:
+        print('FRI')
+    elif Y%7 == 3:
+        print('SAT')
+    else:
+        print('SUN')
+elif X == 4:
+    if Y%7 == 2:
+        print('MON')
+    elif Y%7 == 3:
+        print('TUE')
+    elif Y%7 == 4:
+        print('WED')
+    elif Y%7 == 5:
+        print('THU')
+    elif Y%7 == 6:
+        print('FRI')
+    elif Y%7 == 0:
+        print('SAT')
+    else:
+        print('SUN')
+elif X == 5:
+    if Y%7 == 0:
+        print('MON')
+    elif Y%7 == 1:
+        print('TUE')
+    elif Y%7 == 2:
+        print('WED')
+    elif Y%7 == 3:
+        print('THU')
+    elif Y%7 == 4:
+        print('FRI')
+    elif Y%7 == 5:
+        print('SAT')
+    else:
+        print('SUN')
+elif X == 6:
+    if Y%7 == 4:
+        print('MON')
+    elif Y%7 == 5:
+        print('TUE')
+    elif Y%7 == 6:
+        print('WED')
+    elif Y%7 == 0:
+        print('THU')
+    elif Y%7 == 1:
+        print('FRI')
+    elif Y%7 == 2:
+        print('SAT')
+    else:
+        print('SUN')
+elif X == 7:
+    if Y%7 == 2:
+        print('MON')
+    elif Y%7 == 3:
+        print('TUE')
+    elif Y%7 == 4:
+        print('WED')
+    elif Y%7 == 5:
+        print('THU')
+    elif Y%7 == 6:
+        print('FRI')
+    elif Y%7 == 0:
+        print('SAT')
+    else:
+        print('SUN')
+elif X == 8:
+    if Y%7 == 6:
+        print('MON')
+    elif Y%7 == 0:
+        print('TUE')
+    elif Y%7 == 1:
+        print('WED')
+    elif Y%7 == 2:
+        print('THU')
+    elif Y%7 == 3:
+        print('FRI')
+    elif Y%7 == 4:
+        print('SAT')
+    else:
+        print('SUN')
+elif X == 9:
+    if Y%7 == 3:
+        print('MON')
+    elif Y%7 == 4:
+        print('TUE')
+    elif Y%7 == 5:
+        print('WED')
+    elif Y%7 == 6:
+        print('THU')
+    elif Y%7 == 0:
+        print('FRI')
+    elif Y%7 == 1:
+        print('SAT')
+    else:
+        print('SUN')
+elif X == 10:
+    if Y%7 == 1:
+        print('MON')
+    elif Y%7 == 2:
+        print('TUE')
+    elif Y%7 == 3:
+        print('WED')
+    elif Y%7 == 4:
+        print('THU')
+    elif Y%7 == 5:
+        print('FRI')
+    elif Y%7 == 6:
+        print('SAT')
+    else:
+        print('SUN')
+elif X == 11:
+    if Y%7 == 5:
+        print('MON')
+    elif Y%7 == 6:
+        print('TUE')
+    elif Y%7 == 0:
+        print('WED')
+    elif Y%7 == 1:
+        print('THU')
+    elif Y%7 == 2:
+        print('FRI')
+    elif Y%7 == 3:
+        print('SAT')
+    else:
+        print('SUN')
+else:
+    if Y%7 == 3:
+        print('MON')
+    elif Y%7 == 4:
+        print('TUE')
+    elif Y%7 == 5:
+        print('WED')
+    elif Y%7 == 6:
+        print('THU')
+    elif Y%7 == 0:
+        print('FRI')
+    elif Y%7 == 1:
+        print('SAT')
+    else:
+        print('SUN')
