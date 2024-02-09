c07d002ea687e3bd3ca85082cabccf899c95d228
pluto7 <141066610+taebinchoi@users.noreply.github.com>
2024-02-09 12:09:21 +0900

    [Bronze V] Title: 학점계산, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,28 @@
+G = input()
+
+if G == "A+":
+    print('4.3')
+if G == "A0":
+    print('4.0')
+if G == "A-":
+    print('3.7')
+if G == "B+":
+    print('3.3')
+if G == "B0":
+    print('3.0')        
+if G == "B-":
+    print('2.7')
+if G == "C+":
+    print('2.3')
+if G == "C0":
+    print('2.0')
+if G == "C-":
+    print('1.7')
+if G == "D+":
+    print('1.3')
+if G == "D0":
+    print('1.0')
+if G == "D-":
+    print('0.7')
+if G == "F":
+    print('0.0')
