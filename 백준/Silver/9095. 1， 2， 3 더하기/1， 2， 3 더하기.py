1734390c09cfa4782956786887d957cb0dcd252e
pluto7 <141066610+taebinchoi@users.noreply.github.com>
2024-02-10 20:08:58 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,23 @@
+T = int(input())
+for i in range(T):
+    N = int(input())
+    if N == 1:
+        print(1)
+    if N == 2:
+        print(2)
+    if N == 3:
+        print(4)
+    if N == 4:
+        print(7)
+    if N == 5:
+        print(13)
+    if N == 6:
+        print(24)
+    if N == 7:
+        print(44)
+    if N == 8:
+        print(81)
+    if N == 9:
+        print(149)
+    if N == 10:
+        print(274)
