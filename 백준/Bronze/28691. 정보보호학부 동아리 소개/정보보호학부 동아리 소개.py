2ea6b8149d34cfd160187cbeb87927500fd73bb2
pluto7 <141066610+taebinchoi@users.noreply.github.com>
2024-02-11 22:00:15 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,12 @@
+name = input()
+
+if name == "M":
+    print('MatKor')
+if name == "W":
+    print('WiCys')
+if name == "C":
+    print('CyKor')
+if name == "A":
+    print('AlKor')
+if name == "$":
+    print('$clear')
