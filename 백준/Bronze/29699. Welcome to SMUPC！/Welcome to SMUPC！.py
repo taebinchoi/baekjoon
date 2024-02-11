022e08d5823acc2ae31a92df1ab479a5109aae75
pluto7 <141066610+taebinchoi@users.noreply.github.com>
2024-02-11 22:00:44 +0900

    [Bronze V] Title: Welcome to SMUPC!, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/29699. Welcome to SMUPC！/Welcome to SMUPC！.py
@@ -0,0 +1,30 @@
+N = int(input())
+
+if N % 14 == 0:
+    print('C')
+elif N % 14 == 1:
+    print('W')
+elif N % 14 == 2:
+    print('e')
+elif N % 14 == 3:
+    print('l')
+elif N % 14 == 4:
+    print('c')
+elif N % 14 == 5:
+    print('o')
+elif N % 14 == 6:
+    print('m')
+elif N % 14 == 7:
+    print('e')
+elif N % 14 == 8:
+    print('T')
+elif N % 14 == 9:
+    print('o')
+elif N % 14 == 10:
+    print('S')
+elif N % 14 == 11:
+    print('M')
+elif N % 14 == 12:
+    print('U')
+else:
+    print('P')
