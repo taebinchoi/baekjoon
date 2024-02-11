6fbb2c4db86f6bcf2c5689caef12c0366e075b4a
pluto7 <141066610+taebinchoi@users.noreply.github.com>
2024-02-11 21:35:24 +0900

    [Bronze V] Title: 아스키 코드, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.py
@@ -0,0 +1,126 @@
+wo = input()
+
+if wo == 'A':
+    print('65')
+if wo == 'B':
+    print('66')
+if wo == 'C':
+    print('67')
+if wo == 'D':
+    print('68')
+if wo == 'E':
+    print('69')
+if wo == 'F':
+    print('70')
+if wo == 'G':
+    print('71')
+if wo == 'H':
+    print('72')
+if wo == 'I':
+    print('73')
+if wo == 'J':
+    print('74')
+if wo == 'K':
+    print('75')
+if wo == 'L':
+    print('76')
+if wo == 'M':
+    print('77')
+if wo == 'N':
+    print('78')
+if wo == 'O':
+    print('79')
+if wo == 'P':
+    print('80')
+if wo == 'Q':
+    print('81')
+if wo == 'R':
+    print('82')
+if wo == 'S':
+    print('83')
+if wo == 'T':
+    print('84')
+if wo == 'U':
+    print('85')
+if wo == 'V':
+    print('86')
+if wo == 'W':
+    print('87')
+if wo == 'X':
+    print('88')
+if wo == 'Y':
+    print('89')
+if wo == 'Z':
+    print('90')
+if wo == 'a':
+    print('97')
+if wo == 'b':
+    print('98')
+if wo == 'c':
+    print('99')
+if wo == 'd':
+    print('100')
+if wo == 'e':
+    print('101')
+if wo == 'f':
+    print('102')
+if wo == 'g':
+    print('103')
+if wo == 'h':
+    print('104')
+if wo == 'i':
+    print('105')
+if wo == 'j':
+    print('106')
+if wo == 'k':
+    print('107')
+if wo == 'l':
+    print('108')
+if wo == 'm':
+    print('109')
+if wo == 'n':
+    print('110')
+if wo == 'o':
+    print('111')
+if wo == 'p':
+    print('112')
+if wo == 'q':
+    print('113')
+if wo == 'r':
+    print('114')
+if wo == 's':
+    print('115')
+if wo == 't':
+    print('116')
+if wo == 'u':
+    print('117')
+if wo == 'v':
+    print('118')
+if wo == 'w':
+    print('119')
+if wo == 'x':
+    print('120')
+if wo == 'y':
+    print('121')
+if wo == 'z':
+    print('122')
+if wo == '0':
+    print('48')
+if wo == '1':
+    print('49')
+if wo == '2':
+    print('50')
+if wo == '3':
+    print('51')
+if wo == '4':
+    print('52')
+if wo == '5':
+    print('53')
+if wo == '6':
+    print('54')
+if wo == '7':
+    print('55')
+if wo == '8':
+    print('56')
+if wo == '9':
+    print('57')
