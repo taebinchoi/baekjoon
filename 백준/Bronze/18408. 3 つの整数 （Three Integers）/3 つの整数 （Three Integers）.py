bef3a88d719d5cb15b19c4dabfe71b7afa791983
pluto7 <141066610+taebinchoi@users.noreply.github.com>
2024-02-11 21:44:53 +0900

    [Bronze IV] Title: 3 つの整数 (Three Integers), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/18408. 3 つの整数 （Three Integers）/3 つの整数 （Three Integers）.py
@@ -0,0 +1,5 @@
+A, B ,C = map(int,input().split())
+if (A == 1 and B == 1) or (A == 1 and C == 1) or (B == 1 and C == 1):
+    print(1)
+if (A == 2 and B == 2) or (A == 2 and C == 2) or (B == 2 and C == 2):
+    print(2)
