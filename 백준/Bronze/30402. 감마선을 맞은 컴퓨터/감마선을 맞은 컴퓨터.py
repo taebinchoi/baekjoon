29a14f622d351cd4a4ac28f60630c0ccd6d7bb6a
pluto7 <141066610+taebinchoi@users.noreply.github.com>
2024-02-11 22:02:55 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,11 @@
+photo = []
+for i in range(15):
+    N = input().split()
+    photo.extend(N)
+
+if 'w' in photo:
+    print('chunbae')
+if 'b' in photo:
+    print('nabi')
+if 'g' in photo:
+    print('yeongcheol')
